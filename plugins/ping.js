0458ccb766a4973ea219baeb526447803707a2bc
Pritam <151441883+officialpritam07@users.noreply.github.com>
2024-08-29 00:45:37 +0530

    Update ping.js

--- plugins/ping.js
+++ plugins/ping.js
@@ -10,6 +10,6 @@ bot(
     const start = new Date().getTime()
     await message.send('```Ping!```')
     const end = new Date().getTime()
-    return await message.send('*Pong!*\n ```' + (end - start) + '``` *ms*')
+    return await message.send('*Speed*\n ```' + (end - start) + '``` *ms*')
   }
 )
